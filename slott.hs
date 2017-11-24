a0779dc88a27a502768e742a7a0a7965436f2ffe
liuyangshan <laura.liu177@gmail.com>
2017-11-24 11:34:25 -0800

    with two errors

--- slott.hs
+++ slott.hs
@@ -6,13 +6,14 @@ import System.Random
 
 main = do 
  displayHomePage
- credit <- getLine
- putStrLn("Your credit is: $ " ++ credit )
- putStrLn("Now enter <spin> to play")
+ totalCredit <- getLine
+ putStrLn("Your total credits is: $ " ++ totalCredit )
+ putStrLn("Enter how many credits you want to bet on each game")
+ oneGameCredit <- getLine
+
+ putStrLn("Now enter <spin> to play, <end> to end the game")
  option <- getLine
- -- result <- play option
- print(play option)
- -- putStrLn("Your spin: " ++ )
+ play option totalCredit oneGameCredit
 
 
 
@@ -33,45 +34,37 @@ displayHomePage = mapM_ putStrLn $
  "etc....":
  "":
  "":
- "Now please enter the credit you want to play:":
+ "Now please enter the total credit you want to play:":
  []
 
 
-play option
- | option == "spin" = (evalSpin, evalSpin, evalSpin)
- | otherwise = (0,0,0)
- 	where (evalSpin,_) = randomR (0, 9) getStdGen:: (Int, StdGen)
-  -- return randNum
+play option totalCredit oneGameCredit
+ | totalCredit == 0 = do 
+ 	putStrLn ("You have no more credits")
+
+ | option == "end" = do
+ 	putStrLn ("Game Over")
+
+ | option == "spin" = do
+ 	let newTotalCredit = totalCredit - oneGameCredit
+ 	gen <- getStdGen
+
+	let (evalSpin1,_) = randomR (0, 9) gen:: (Int, StdGen)
+	let (evalSpin2,_) = randomR (0, 9) gen:: (Int, StdGen)
+	let (evalSpin3,_) = randomR (0, 9) gen:: (Int, StdGen)
+
+ 	putStrLn ("reel 1: " ++ show evalSpin1)
+ 	putStrLn ("reel 2: " ++ show evalSpin2)
+ 	putStrLn ("reel 3: " ++ show evalSpin3)
+
+ 	putStrLn("Enter how many credits you want to bet on each game")
+ 	newOneGameCredit <- getLine
+ 	putStrLn("Now enter <spin> to play, <end> to end the game")
+ 	newOption <- getLine
+ 	play newOption newTotalCredit newOneGameCredit
 
 
 
-
- -- evalSpin = do
- --  -- gen <- getStdGen
- --  let (randNum,_) <-  randomR (0, 9) getStdGen:: (Int, stdGen)
- --  return randNum
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ | otherwise = do
+ 	putStrLn ("Invaild Input")
 
