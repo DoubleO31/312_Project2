eb3e9cfeb7ce2ae768cdaf308878ac069ce5d82d
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2017-11-27 19:10:21 -0800

    edited to be readable by main

--- Guess.hs
+++ Guess.hs
@@ -1,17 +1,18 @@
+module Guess where
 import System.IO 
 import System.Random
 
-main = do 
+guess = do 
  handle <- openFile "words.txt" ReadMode
  contents <- hGetContents handle
  gen <- getStdGen
  let words = lines contents
  let (n, _ ) = randomR (0, (length words) - 1)  gen :: (Int, StdGen)
  let word = words !! n
- play word ( map (\ x -> '_') word ) 6
+ playguess word ( map (\ x -> '_') word ) 6
  hClose handle
 
-play word known guesses
+playguess word known guesses
  | word == known = do
  	putStrLn known
 	putStrLn ("You win!")
@@ -23,9 +24,9 @@ play word known guesses
 	putStrLn ("You have " ++ (show guesses) ++ " guesses left.")
 	line <- getLine
 	let (newKnown, newGuesses) = handle (head line) word known guesses
-	play word newKnown newGuesses
+	playguess word newKnown newGuesses
 
 
 handle letter word known guesses
- | letter `elem` word = ( zipWith (\ w k -> if w == letter then w else k) word known, guesses)
+ | letter `elem` word = ( zipWith (\ w k -> if w == letter then w else k) word known, guesses-1)
  | otherwise = (known, guesses - 1)
