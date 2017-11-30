93063b27e862527de9967ada7cfedbfabd450587
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2017-11-29 17:58:39 -0800

    added rewards

--- Guess.hs
+++ Guess.hs
@@ -3,32 +3,31 @@ import System.IO
 import System.Random
 
 
-guess = do 
+guess gen5= do 
  handle <- openFile "words.txt" ReadMode
  contents <- hGetContents handle
- gen <- getStdGen
  let words = lines contents
- let (n, _ ) = randomR (0, (length words) - 1)  gen :: (Int, StdGen)
+ let (n, _ ) = randomR (0, (length words) - 1)  gen5 :: (Int, StdGen)
  let word = words !! n
  playguess word ( map (\ x -> '_') word ) 6
- hClose handle
 
-playguess word known guesses
+playguess word known guesses 
  | word == known = do
- 	putStrLn known
-	putStrLn ("You win!")
-	
+   putStrLn known
+   putStrLn ("You win!")
+   return 2
  | guesses == 0 = do
-	putStrLn known
-	putStrLn ("You lose. The word was " ++ word ++ ".")
+   putStrLn known
+   putStrLn ("You lose. The word was " ++ word ++ ".")
+   return 0
  | otherwise = do
-  	putStrLn known
-	putStrLn ("You have " ++ (show guesses) ++ " guesses left.")
-	line <- getLine
-	let (newKnown, newGuesses) = handle (head line) word known guesses
-	playguess word newKnown newGuesses
+   putStrLn known
+   putStrLn ("You have " ++ (show guesses) ++ " guesses left.")
+   line <- getLine
+   let (newKnown, newGuesses) = handle (head line) word known guesses
+   playguess word newKnown newGuesses
 
 
 handle letter word known guesses
  | letter `elem` word = ( zipWith (\ w k -> if w == letter then w else k) word known, guesses-1)
- | otherwise = (known, guesses - 1)
+ | otherwise = (known, guesses)
