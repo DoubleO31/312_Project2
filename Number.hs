a1cea12ed9f7b7f548ddbe5119c9ba4f2d0bd9d6
liuyangshan <laura.liu177@gmail.com>
2017-11-30 23:12:04 -0800

    display added

--- Number.hs
+++ Number.hs
@@ -5,18 +5,45 @@ import Text.Read
 
 
 guessnumber genX= do
+ displayHelp
+
  let (randomnumber,_) = randomR (1, 100) genX:: (Int, StdGen)
  playguessnumber randomnumber 0 6
 
+
+displayHelp = mapM_ putStrLn $
+ "":
+ "":
+ "  ***********************************************************************":
+ "  ************************ GUESS THE MAGIC NUMBER ***********************":
+ "  ***********************************************************************":
+ "":
+ "":
+ "                           Little insides:":
+ "                 You have 6 chances to guess the number":
+ "                     The number is between 0 - 100 ":
+ "":
+ "":
+ "":
+ "                             How to Play:":
+ "                      Type your number and Enter!":
+ "":
+ "                             LET'S START":
+ "":
+ "":
+ []
+
+
+
 playguessnumber number guessn guesses 
  | number == guessn = do
-   putStrLn ("You win!")
+   putStrLn ("  You win!")
    return 2
  | guesses == 0 = do
-   putStrLn ("You lose. The number was " ++ (show number) ++ ".")
+   putStrLn ("  You lose. The number was " ++ (show number) ++ ".")
    return 0
  | otherwise = do
-   putStrLn ("You have " ++ (show guesses) ++ " guesses left.")
+   putStrLn ("  You have " ++ (show guesses) ++ " guesses left.")
    line <- getLineInt
    handle number line guesses
 
@@ -24,7 +51,7 @@ playguessnumber number guessn guesses
 handle number guessn guesses
  | guessn > number = do
   putStrLn(" ")
-  putStrLn ("Your guess number is too high!")
+  putStrLn ("  Your guess number is too high!")
   putStrLn(" ")
   let newguesses = guesses - 1
   playguessnumber number guessn newguesses
@@ -34,7 +61,7 @@ handle number guessn guesses
   
  | otherwise = do
   putStrLn(" ")
-  putStrLn ("Your guess number is too low!")
+  putStrLn ("  Your guess number is too low!")
   putStrLn(" ")
   let newguesses = guesses - 1
   playguessnumber number guessn newguesses
@@ -43,8 +70,8 @@ handle number guessn guesses
 
 getLineInt :: IO Int
 getLineInt = do
-  putStrLn "Now please enter a number ranging from 0 to 100:"
+  putStrLn "  Now please enter a number (range between 0 to 100) :"
   line <- getLine
   case readMaybe line of
     Just x -> return x
-    Nothing -> putStrLn "Invalid number entered" >> getLineInt
+    Nothing -> putStrLn "  Invalid number entered" >> getLineInt
