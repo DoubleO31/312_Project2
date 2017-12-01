48bc8833cdeb3cd18206215caea4c01b94d2dc68
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2017-11-30 21:48:51 -0800

    Change name to word and fixed empty list exception

--- Word.hs
+++ Word.hs
@@ -1,17 +1,17 @@
-module Guess where
+module Word where
 import System.IO 
 import System.Random
 
 
-guess gen5= do 
+guessword genX= do 
  handle <- openFile "words.txt" ReadMode
  contents <- hGetContents handle
  let words = lines contents
- let (n, _ ) = randomR (0, (length words) - 1)  gen5 :: (Int, StdGen)
+ let (n, _ ) = randomR (0, (length words) - 1)  genX :: (Int, StdGen)
  let word = words !! n
- playguess word ( map (\ x -> '_') word ) 5
+ playguessword word ( map (\ x -> '_' ) word ) 6
 
-playguess word known guesses 
+playguessword word known guesses 
  | word == known = do
    putStrLn known
    putStrLn ("You win!")
@@ -21,13 +21,19 @@ playguess word known guesses
    putStrLn ("You lose. The word was " ++ word ++ ".")
    return 0
  | otherwise = do
+   putStrLn(" ")
+   putStrLn ("A " ++ (show(length known)) ++ " letters word.")
    putStrLn known
    putStrLn ("You have " ++ (show guesses) ++ " guesses left.")
+   putStrLn(" ")
    line <- getLine
-   let (newKnown, newGuesses) = handle (head line) word known guesses
-   playguess word newKnown newGuesses
+   if length line == 0 
+   then do playguessword word known guesses 
+   else do
+    let (newKnown, newGuesses) = handle (head line) word known guesses
+    playguessword word newKnown newGuesses
 
 
 handle letter word known guesses
  | letter `elem` word = ( zipWith (\ w k -> if w == letter then w else k) word known, guesses)
- | otherwise = (known, guesses-1)
+ | otherwise = (known, guesses -1)
